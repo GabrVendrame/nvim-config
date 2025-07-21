191fabba66f380f7a92f3021f2cb40aa04372496
GabrVendrame <gabrielvendrame7@gmail.com>
2025-07-21 18:04:23 -0300

    feat[lazydev.lua]: adds opts

--- lua/gabrvendrame/plugins/lazydev.lua
+++ lua/gabrvendrame/plugins/lazydev.lua
@@ -1,6 +1,14 @@
 return {
         {
                 "folke/lazydev.nvim",
-                opts = {}
+                ft = 'lua',
+                opts = {
+                        library = {
+                                {
+                                        path = "${3rd}/luv/library",
+                                        words = { "vim%.uv" },
+                                }
+                        }
+                }
         },
 }
