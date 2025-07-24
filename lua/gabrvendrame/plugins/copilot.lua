8a8b2e6f0bc10ee541f94b177fc68cdb776ddf8c
GabrVendrame <gabrielvendrame7@gmail.com>
2025-07-24 19:08:45 -0300

    feat[copilot.lua]: remaps accept suggestion binding

--- lua/gabrvendrame/plugins/copilot.lua
+++ lua/gabrvendrame/plugins/copilot.lua
@@ -1,13 +1,21 @@
+local function remap(mode, lhs, rhs, desc, expr, silent)
+        local opts = {
+                desc = desc or nil,
+                expr = expr or false,
+                silent = silent or false,
+        }
+        vim.keymap.set(mode, lhs, rhs, opts)
+end
+
 return {
         {
                 "github/copilot.vim",
                 config = function()
-                        local function remap(mode, lhs, rhs, desc)
-                                vim.keymap.set(mode, lhs, rhs, { desc = desc or nil })
-                        end
                         remap("n", "<leader>cst", "<cmd>Copilot status<cr>", "Copilot status")
                         remap("n", "<leader>con", "<cmd>Copilot enable<cr>", "Turn on copilot")
                         remap("n", "<leader>cof", "<cmd>Copilot disable<cr>", "Turn off copilot")
+                        remap("i", "<C-a>", "copilot#Accept('\\<CR>')", "Accept Copilot suggestion", true, true)
+                        vim.g.copilot_no_tab_map = true
                 end
         }
 }
