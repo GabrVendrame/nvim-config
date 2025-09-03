f87e642f742e216ac04bdaab21ef4512371cc476
GabrVendrame <gabrielvendrame7@gmail.com>
2025-09-03 20:11:14 -0300

    refactor: return to the native diagnostics lsp

--- lua/gabrvendrame/plugins/telescope.lua
+++ lua/gabrvendrame/plugins/telescope.lua
@@ -45,9 +45,11 @@ return {
                 vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Find git files" })
                 vim.keymap.set("n", "<leader>ps", builtin.grep_string, { desc = "Search current word" })
                 vim.keymap.set("n", "<leader>pw", builtin.live_grep, { desc = "Search by grep" })
-
-                vim.keymap.set("n", "<leader>fvim", function()
-                        builtin.find_files { cwd = vim.fn.stdpath "config" }
-                end, { desc = "Search in neovim configuration" })
+                vim.keymap.set("n", "<leader>vrr", builtin.lsp_references, { desc = "Find references" })
+                vim.keymap.set("n", "<leader>vws", builtin.lsp_workspace_symbols, { desc = "Workspace symbol" })
+                vim.keymap.set("n", "<leader>q", builtin.diagnostics, { desc = "List diagnostics" })
+                vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = "Go to definition" })
+                vim.keymap.set("n", "<leader>gi", builtin.lsp_implementations, { desc = "Go to implementantions" })
+                vim.keymap.set("n", "<leader>st", builtin.git_status, { desc = "Git status on telescope" })
         end,
 }
--- lua/gabrvendrame/utils.lua
+++ lua/gabrvendrame/utils.lua
@@ -9,16 +9,13 @@ local function client_supports_method(client, method, bufnr)
 end
 
 function U.set_keymaps(bufnr)
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition", buffer = bufnr })
         vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation", buffer = bufnr })
-        vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, { desc = "Workspace symbol", buffer = bufnr })
         vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, { desc = "Show diagnostics", buffer = bufnr })
         vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = 1, float = true }) end,
                 { desc = "Next diagnostic", buffer = bufnr })
         vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = -1, float = true }) end,
                 { desc = "Previous diagnostic", buffer = bufnr })
         vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, { desc = "Code action", buffer = bufnr })
-        vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, { desc = "Find references", buffer = bufnr })
         vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, { desc = "Rename symbol", buffer = bufnr })
         vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, { desc = "Signature help", buffer = bufnr })
 end
